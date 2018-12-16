7af5d1c9f841509ef184975f900c37f996b6502a
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2018-12-16 20:21:59 +0700

    Fix Scroll

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+
+        $("#navigation-button").click(function () {
+            $("#notification-number").toggleClass("notification-hide");
+            $(".nav-bar").toggleClass("nav-bar-hide");
+            $(".bgblue").toggleClass("bgblue-show");
+            $(".hamburger").toggleClass("is-active");
+        });
+
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+
+        $(function () {
+            $.scrollify({
+                section: ".scroll-web",
+            });
+
+        });
