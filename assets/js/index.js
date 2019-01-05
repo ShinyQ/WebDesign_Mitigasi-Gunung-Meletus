4059c186cea33cfd8d6a0c4fb1cd5f3ca5aa3ad7
Adrian Hafizh <adrian_aryaputra_25rpl@student.smktelkom-mlg.sch.id>
2019-01-05 18:52:33 +0700

    fixing responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,25 @@
+$("#navigation-button").click(function () {
+    $("#notification-number").toggleClass("notification-hide");
+    $(".nav-bar").toggleClass("nav-bar-hide");
+    $(".bgblue").toggleClass("bgblue-show");
+    $(".hamburger").toggleClass("is-active");
+});
 
-        $("#navigation-button").click(function () {
-            $("#notification-number").toggleClass("notification-hide");
-            $(".nav-bar").toggleClass("nav-bar-hide");
-            $(".bgblue").toggleClass("bgblue-show");
-            $(".hamburger").toggleClass("is-active");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+
         });
-
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-            anchor.addEventListener('click', function (e) {
-                e.preventDefault();
-        
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                    
+    });
+});
+$(window).scroll(function () {
+            if ($(window).scrollTop() >= $(".wrappermain").offset().top - 120) {
+                $(".navbar").css({
+                    "color": "white",
+                    "opacity":"1",
                 });
-            });
+            }
         });
